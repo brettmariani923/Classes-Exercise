64c8be89675e70c12c65248443f2df32b611a8e4
Brett Mariani <brettmariani923@gmail.com>
2025-01-10 01:30:56 -0600

    first commit

---
+++ Classes/Car.cs
@@ -0,0 +1,10 @@
+﻿namespace Classes;
+
+public class Car
+{
+    public int Year;  // Field		
+
+    public string Make {get; set;}
+
+    public string Model {get; set;} 
+}
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Car newCar = new Car();
+
+            newCar.Make = "Jeep";
+            newCar.Model = "Cherokee";
+            newCar.Year = 1999;
+            
+            Console.WriteLine($"Make: {newCar.Make}");
+            Console.WriteLine($"Model: {newCar.Model}");
+            Console.WriteLine($"Year: {newCar.Year}");
         }
     }
 }
